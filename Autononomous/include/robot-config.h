fe0372c7e68b75c4f31d94374e06dd32cb86ff21
NelTheCoder <geometrydashneil@gmail.com>
2019-10-16 14:34:52 -0700

    Added Autononomous code
    added autononomous code what else do you think we did

---
+++ Autononomous/include/robot-config.h
@@ -0,0 +1,17 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern controller Controller1;
+extern motor Bottom_left;
+extern motor Bottom_right;
+extern motor Top_right;
+extern motor Top_left;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Text.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
---
+++ Autononomous/src/main.cpp
@@ -0,0 +1,100 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\neiln                                            */
+/*    Created:      Wed Oct 16 2019                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Controller1          controller                    
+// Bottom_left          motor         12              
+// Bottom_right         motor         11              
+// Top_right            motor         13              
+// Top_left             motor         14              
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+/*
+Set the motors to the correct ports
+*/
+void lefty(double amount)
+{
+    Top_left.rotateFor(amount, rotationUnits::deg, 100, velocityUnits::pct, false);
+    Bottom_left.rotateFor(amount, rotationUnits::deg, 100, velocityUnits::pct, false);
+    Top_right.rotateFor(amount, rotationUnits::deg, 100, velocityUnits::pct, false);
+    Bottom_left.rotateFor(amount, rotationUnits::deg, 100, velocityUnits::pct, true);
+}
+void backwardy(double amount)
+{
+    Top_left.rotateFor(-amount, rotationUnits::deg, 100, velocityUnits::pct, false);
+    Bottom_left.rotateFor(-amount, rotationUnits::deg, 100, velocityUnits::pct, false);
+    Top_right.rotateFor(amount, rotationUnits::deg, 100, velocityUnits::pct, false);
+    Bottom_left.rotateFor(amount, rotationUnits::deg, 100, velocityUnits::pct, true);
+}
+void righty(double amount)
+{
+    Top_left.rotateFor(amount, rotationUnits::deg, 100, velocityUnits::pct, false);
+    Bottom_left.rotateFor(-amount, rotationUnits::deg, 100, velocityUnits::pct, false);
+    Top_right.rotateFor(-amount, rotationUnits::deg, 100, velocityUnits::pct, false);
+    Bottom_left.rotateFor(amount, rotationUnits::deg, 100, velocityUnits::pct, true);
+}
+void forwardy(double amount)
+{
+    Top_left.rotateFor(-amount, rotationUnits::deg, 100, velocityUnits::pct, false);
+    Bottom_left.rotateFor(amount, rotationUnits::deg, 100, velocityUnits::pct, false);
+    Top_right.rotateFor(amount, rotationUnits::deg, 100, velocityUnits::pct, false);
+    Bottom_right.rotateFor(-amount, rotationUnits::deg, 100, velocityUnits::pct, true);
+}
+void victory_dance()
+{
+    Top_left.startRotateFor(720,rotationUnits::deg,100,velocityUnits::pct);
+    Bottom_left.startRotateFor(720,rotationUnits::deg,100,velocityUnits::pct);
+    Top_right.startRotateFor(720,rotationUnits::deg,100,velocityUnits::pct);
+    Bottom_right.startRotateFor(720,rotationUnits::deg,100,velocityUnits::pct);
+    Brain.Screen.print("V-I-C-T-O-R-Y--D-A-N-C-E");
+}
+void autonomous()
+{
+    lefty(20);
+    forwardy(360);
+    righty(105);
+    forwardy(720);
+    lefty(45);
+    forwardy(360);
+    righty(75);
+    forwardy(1080);
+    victory_dance();
+}
+int main() 
+{
+    while(true)
+    {
+        Controller1.ButtonL1.pressed(autonomous);
+        if(Controller1.ButtonUp.pressing())
+        {
+            forwardy(360);
+            task::sleep(100);
+        }
+        else if(Controller1.ButtonRight.pressing())
+        {
+            righty(360);
+            task::sleep(100);
+        }
+        else if(Controller1.ButtonLeft.pressing())
+        {
+            lefty(360);
+            task::sleep(100);
+        }
+        else if(Controller1.ButtonDown.pressing())
+        {
+            backwardy(360);
+            task::sleep(100);
+        }
+    }
+}
+
