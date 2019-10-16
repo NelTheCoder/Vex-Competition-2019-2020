da994bfc1eb009ba8f6441f493233033a153999c
NelTheCoder <geometrydashneil@gmail.com>
2019-10-16 09:57:34 -0700

    Added Driver Code

---
+++ v5code files/Driving_Code/include/robot-config.h
@@ -0,0 +1,19 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern controller Controller1;
+extern motor Motor11;
+extern motor Motor12;
+extern motor Motor13;
+extern motor Motor14;
+extern motor Lift_left;
+extern motor Lift_right;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Text.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
---
+++ v5code files/Driving_Code/src/main.cpp
@@ -0,0 +1,30 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\neiln                                            */
+/*    Created:      Wed Oct 16 2019                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Controller1          controller                    
+// Motor11              motor         11              
+// Motor12              motor         12              
+// Motor13              motor         13              
+// Motor14              motor         14              
+// Lift_left            motor         1               
+// Lift_right           motor         2               
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}
