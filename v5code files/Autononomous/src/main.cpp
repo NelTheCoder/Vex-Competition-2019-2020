6125ca52f75096f36a24bc9e4839f7493fd43361
NelTheCoder <geometrydashneil@gmail.com>
2019-10-16 12:50:56 -0700

    Added Autonomous Code
    basically what the title says

---
+++ v5code files/Autononomous/src/main.cpp
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\neiln                                            */
+/*    Created:      Wed Oct 16 2019                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}
