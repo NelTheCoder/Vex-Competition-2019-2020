4d7d7472340bf0a0b28af1f180875e19019289b5
NelTheCoder <geometrydashneil@gmail.com>
2019-10-18 11:48:45 -0700

    Update lift_testing.h

--- v5code files/Driving_Code/src/lift_testing.h
+++ v5code files/Driving_Code/src/lift_testing.h
@@ -4,9 +4,6 @@ using namespace vex;
 
 void lift()
 {
-  if(Controller1.ButtonL1.pressing())
-  {
-    Lift_left.spin(directionType::fwd, Controller1.Axis3.value(), velocityUnits::pct);
-    Lift_right.spin(directionType::fwd, Controller1.Axis3.value(), velocityUnits::pct);
-  }
+  Lift_left.spin(directionType::fwd, Controller1.Axis3.value(), velocityUnits::pct);
+  Lift_right.spin(directionType::fwd, Controller1.Axis3.value(), velocityUnits::pct);
 }
