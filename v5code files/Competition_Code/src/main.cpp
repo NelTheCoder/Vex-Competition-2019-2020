cac6716ac670eda7fdd0813347e6b6482b6f6544
NelTheCoder <geometrydashneil@gmail.com>
2019-10-19 09:17:50 -0700

    added in competition stuff
    put in the competition fucntions and removed comments. i am them god of coding in our team

--- v5code files/Competition_Code/src/main.cpp
+++ v5code files/Competition_Code/src/main.cpp
@@ -14,8 +14,38 @@
 
 using namespace vex;
 
-int main() {
+competition Competition;
+
+void pre_auton(void) 
+{
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
-  
+}
+
+void autonomous(void) 
+{
+
+}
+
+void usercontrol(void) 
+{
+  while (1) 
+  {
+    wait(20, msec);
+  }
+}
+
+int main() 
+{
+  // Set up callbacks for autonomous and driver control periods.
+  Competition.autonomous(autonomous);
+  Competition.drivercontrol(usercontrol);
+
+  // Run the pre-autonomous function.
+  pre_auton();
+
+  // Prevent main from exiting with an infinite loop.
+  while (true) {
+    wait(100, msec);
+  }
 }
