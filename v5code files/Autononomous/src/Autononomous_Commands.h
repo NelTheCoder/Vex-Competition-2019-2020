53d1fac13d7b6d5a0116ef701ca0f0dcfe3f2378
NelTheCoder <geometrydashneil@gmail.com>
2019-10-19 09:05:43 -0700

    fixed reversed motors
    fixed motors that were reversed. hope nothing broke

--- v5code files/Autononomous/src/Autononomous_Commands.h
+++ v5code files/Autononomous/src/Autononomous_Commands.h
@@ -4,14 +4,17 @@ using namespace vex;
 
 double controlboi = 541;
 
+//turn left
 void lefty(double amount)
 {
+    //make command usable with human degrees
     amount = amount * (controlboi/90);
-    Top_left.rotateFor(amount, rotationUnits::deg, 100, velocityUnits::pct, false);
-    Bottom_left.rotateFor(amount, rotationUnits::deg, 100, velocityUnits::pct, false);
-    Top_right.rotateFor(amount, rotationUnits::deg, 100, velocityUnits::pct, false);
-    Bottom_left.rotateFor(amount, rotationUnits::deg, 100, velocityUnits::pct, true);
+    Top_left.rotateFor(-amount, rotationUnits::deg, 100, velocityUnits::pct, false);
+    Bottom_left.rotateFor(-amount, rotationUnits::deg, 100, velocityUnits::pct, false);
+    Top_right.rotateFor(-amount, rotationUnits::deg, 100, velocityUnits::pct, false);
+    Bottom_left.rotateFor(-amount, rotationUnits::deg, 100, velocityUnits::pct, true);
 }
+//move backwards
 void backwardy(double amount)
 {
     Top_left.rotateFor(-amount, rotationUnits::deg, 100, velocityUnits::pct, false);
@@ -19,26 +22,29 @@ void backwardy(double amount)
     Top_right.rotateFor(amount, rotationUnits::deg, 100, velocityUnits::pct, false);
     Bottom_left.rotateFor(amount, rotationUnits::deg, 100, velocityUnits::pct, true);
 }
+//turn right
 void righty(double amount)
 {
-  amount = amount * (controlboi/90);
+    //make command usable with human degrees
+    amount = amount * (controlboi/90);
     Top_left.rotateFor(amount, rotationUnits::deg, 100, velocityUnits::pct, false);
-    Bottom_left.rotateFor(-amount, rotationUnits::deg, 100, velocityUnits::pct, false);
-    Top_right.rotateFor(-amount, rotationUnits::deg, 100, velocityUnits::pct, false);
-    Bottom_left.rotateFor(amount, rotationUnits::deg, 100, velocityUnits::pct, true);
-}
-void forwardy(double amount)
-{
-    Top_left.rotateFor(-amount, rotationUnits::deg, 100, velocityUnits::pct, false);
     Bottom_left.rotateFor(amount, rotationUnits::deg, 100, velocityUnits::pct, false);
     Top_right.rotateFor(amount, rotationUnits::deg, 100, velocityUnits::pct, false);
+    Bottom_left.rotateFor(amount, rotationUnits::deg, 100, velocityUnits::pct, true);
+}
+//move forward
+void forwardy(double amount)
+{
+    Top_left.rotateFor(amount, rotationUnits::deg, 100, velocityUnits::pct, false);
+    Bottom_left.rotateFor(amount, rotationUnits::deg, 100, velocityUnits::pct, false);
+    Top_right.rotateFor(-amount, rotationUnits::deg, 100, velocityUnits::pct, false);
     Bottom_right.rotateFor(-amount, rotationUnits::deg, 100, velocityUnits::pct, true);
 }
 void victory_dance()
 {
     Top_left.startRotateFor(720,rotationUnits::deg,100,velocityUnits::pct);
     Bottom_left.startRotateFor(720,rotationUnits::deg,100,velocityUnits::pct);
-    Top_right.startRotateFor(720,rotationUnits::deg,100,velocityUnits::pct);
-    Bottom_right.startRotateFor(720,rotationUnits::deg,100,velocityUnits::pct);
+    Top_right.startRotateFor(-720,rotationUnits::deg,100,velocityUnits::pct);
+    Bottom_right.startRotateFor(-720,rotationUnits::deg,100,velocityUnits::pct);
     Brain.Screen.print("V-I-C-T-O-R-Y--D-A-N-C-E");
 }
--- v5code files/Driving_Code/src/driving_functions.h
+++ v5code files/Driving_Code/src/driving_functions.h
@@ -8,20 +8,32 @@ void lift()
   Lift_right.spin(directionType::fwd, Controller1.Axis3.value(), velocityUnits::pct);*/
   if(Controller1.ButtonR1.pressing())
   {
+    //move lift up
     Lift_left.spin(directionType::fwd, 100, velocityUnits::pct);
     Lift_right.spin(directionType::fwd, 100, velocityUnits::pct);
     Lift_extra.spin(directionType::fwd, 100, velocityUnits::pct);
   }
-  if(Controller1.ButtonL1.pressing())
+  else if(Controller1.ButtonL1.pressing())
   {
+    //move lift down
     Lift_left.spin(directionType::rev, 100, velocityUnits::pct);
     Lift_right.spin(directionType::rev, 100, velocityUnits::pct);
     Lift_extra.spin(directionType::rev, 100, velocityUnits::pct);
   }
+  else
+  {
+    //stahp all lift motors 
+    Lift_left.stop();
+    Lift_extra.stop();
+    Lift_right.stop();
+  }
 }
-
+//driving code
 void drive()
 {   
+  //strafing
+  //axis 4 is left joystick, axis 1 is right joystick
+  //if both joysticks are right, strafe right
   if(Controller1.Axis1.value() > 10 && Controller1.Axis4.value() > 10)
   {
       Top_left.spin(directionType::fwd, (Controller1.Axis1.value() + Controller1.Axis4.value()) / 2, velocityUnits::pct);
@@ -29,6 +41,7 @@ void drive()
       Top_right.spin(directionType::fwd, (Controller1.Axis1.value() + Controller1.Axis4.value()) / 2, velocityUnits::pct);
       Bottom_right.spin(directionType::rev, (Controller1.Axis1.value() + Controller1.Axis4.value()) / 2, velocityUnits::pct);
   }
+  //if both joysticks are left, strafe left
   else if(Controller1.Axis1.value() < -10 && Controller1.Axis4.value() < -10)
   {
       Top_left.spin(directionType::fwd, (Controller1.Axis1.value() + Controller1.Axis4.value()) / 2, velocityUnits::pct);
@@ -38,9 +51,11 @@ void drive()
   }
   else
   {
+  //Driving w/ joysticks. Left joystick controls left 2 wheels, Right joystick controls right 2 wheels.
+  //Axis 3 is left joystick, Axis 2 is right joystick
   Bottom_left.spin(directionType::fwd, Controller1.Axis3.value(), velocityUnits::pct);
   Top_left.spin(directionType::fwd, Controller1.Axis3.value(), velocityUnits::pct);
-  Top_right.spin(directionType::fwd, Controller1.Axis2.value() * -1, velocityUnits::pct);
-  Bottom_right.spin(directionType::fwd, Controller1.Axis2.value() * -1, velocityUnits::pct);
+  Top_right.spin(directionType::rev, Controller1.Axis2.value(), velocityUnits::pct);
+  Bottom_right.spin(directionType::rev, Controller1.Axis2.value(), velocityUnits::pct);
   }
 }
