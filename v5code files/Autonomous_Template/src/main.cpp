62ff3a881daa07203b22ca5f245b0b7383e5d637
NelTheCoder <geometrydashneil@gmail.com>
2019-11-14 14:23:25 -0800

    added autonomous template
    autonomous template is meant to be added upon with more accurate values for final autonomous

---
+++ v5code files/Autonomous_Template/src/main.cpp
@@ -0,0 +1,43 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\neiln                                            */
+/*    Created:      Thu Nov 14 2019                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Top_left             motor         11              
+// Bottom_left          motor         13              
+// Top_right            motor         12              
+// Bottom_right         motor         14              
+// Lift_left            motor         1               
+// Lift_right           motor         2               
+// claw                 motor         20              
+// vacuum_left          motor         3               
+// vacuum_right         motor         4               
+// Controller1          controller                    
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main()
+{
+  //stuff
+}
+
+void VaccumAutonomous()
+{
+  //drive forward
+  //use vision sensor to align
+  //start vacuum motors
+  //drive forward while picking up cubes
+  //rotate to face goal zone
+  //drive till very close to goal zone
+  //drop cubes
+}
